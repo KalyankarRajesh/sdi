c3bc11baedbd435169b9a36c314954dbcca8d26a
rajesh-kum-kalyankar <rajesh.kum.kalyankar@microfocus.com>
2019-01-20 11:17:39 +0530

    DI commit

---
+++ SdiApplication.java
@@ -0,0 +1,14 @@
+package com.springframework.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdiApplication.class, args);
+    }
+
+}
+
